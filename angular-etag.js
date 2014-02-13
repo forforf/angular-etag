165915e316e2c124430e33b92b471fe57f2117c3
David Martin <dmarti21@gmail.com>
2014-02-13 18:50:34 -0500

    add comments

--- angular-etag.js
+++ angular-etag.js
@@ -2,26 +2,28 @@
 
 angular.module('AngularEtag', [])
 
-
+// ehttp wraps $http get function and caches etag responses to localStorage
 .factory('ehttp', function($http, $window, $q){
   var STORAGE_PREFIX = 'eTagKey-';
 
+  // wrapper for localStorage
   var storage = {};
   storage.save = function(key, obj){
     var val = JSON.stringify(obj);
     $window.localStorage.setItem(key, val);
   };
-
   storage.get = function(key){
     var val = $window.localStorage.getItem(key);
     return JSON.parse(val);
   };
 
+  //generates key to use for localStorage
   function eTagKey(url){
     if(!url){ return null; }
     return STORAGE_PREFIX+url;
   }
 
+  //creates the object to be cached in localStorage
   function makeCacheObj(etag, opts, response){
     return {
       etag: etag,
@@ -30,6 +32,7 @@ angular.module('AngularEtag', [])
     };
   }
 
+  //caches the etag to local storage. Won't cache unless url exists in opts
   function cacheEtag(etag, opts, resp){
     if(opts && opts.url){
       if(resp){
@@ -42,35 +45,48 @@ angular.module('AngularEtag', [])
     }
   }
 
+  //Wrapper for $http get
   function ehttpGet(urlOpts){
+
+    // Handlers for the server response
+    var respFn = {}
+
+    // cache the Etag prior to returning the reponse
+    respFn.cacheEtag = function(resp){
+      var etag = resp.headers().etag;
+      if(etag){ cacheEtag(etag, urlOpts, resp); }
+      return resp;
+    };
+
+    //304's are treated as exceptions in angular, so
+    //catch it, reject to bubble up the error if not 304
+    respFn.catchUnmodified =  function(resp){
+      if(resp.status === 304){
+        cacheObj.response.headers = function(){
+          return {"X-Local-Cache": "Nothing sent to server"};
+        };
+        return cacheObj.response;
+
+      } else {
+        return $q.reject(resp);
+      }
+    };
+
     var url = urlOpts.url;
 
     var cacheObj = storage.get(eTagKey(url));
 
-    //var eTag = "\"060d3cf9dff002a0f3c7cdaf7ed7ec39\"";
     if(cacheObj && cacheObj.etag){
       urlOpts.headers = urlOpts.headers || {};
       angular.extend( urlOpts.headers, {'If-None-Match': cacheObj.etag} );
     }
+
     angular.extend(urlOpts, {method: 'GET'});
 
+    //$http decorated with etag goodness
     return $http(urlOpts)
-      .then(function(resp){
-        var etag = resp.headers().etag;
-        if(etag){ cacheEtag(etag, urlOpts, resp); }
-        return resp;
-      })
-      .catch(function(resp){
-        if(resp.status === 304){
-          //console.log('storage get', storage.get( cacheObj ));
-          cacheObj.response.headers = function(){
-            return {"X-Local-Cache": "Nothing sent to server"};
-          };
-          return cacheObj.response;
-        } else {
-          return $q.reject(resp);
-        }
-      });
+      .then(respFn.cacheEtag)
+      .catch(respFn.catchUnmodified);
   }
 
 
