c8e6df3caaae12a3f0de28f83ec93e7a51c1a8ca
David Martin <dmarti21@gmail.com>
2014-02-24 20:11:23 -0500

    reorganized code, reduced duplication, updated version and metadata

--- angular-etag.js
+++ angular-etag.js
@@ -8,9 +8,11 @@ var etagUtil = angular.module('etagUtil', []);
 // Some services will decorate or alter the core angular services,
 // So it is important that these utility
 // functions have no dependencies themselves
-etagUtil.factory('etagLocalStorageCache', function(){
-  
-  return function(_http, _window, _q){
+// Note: This separation is not strictly necessary, but I really like
+// this pattern for re-use and readability.
+etagUtil.factory('etagLocalStorageCacheFn', function(){
+
+  return function (_http, _window, _q){
     var STORAGE_PREFIX = 'eTagKey-';
 
     // wrapper for localStorage
@@ -96,107 +98,29 @@ etagUtil.factory('etagLocalStorageCache', function(){
         .then(respFn.cacheEtag)
         .catch(respFn.catchUnmodified);
     }
-
   }
 });
 
-
-angular.module('AngularEtag', [])
+angular.module('AngularEtag', ['etagUtil'])
 
 .provider('etagLocalStorageCache', function(){
+
+    // We don't have any angular magic yet to pull in dependencies.
+    // So we have to manually do it with angular.injector
+    var injector = angular.injector(['etagUtil']);
+
+    //With the injector we can 'get' services from the other module
+    // like the caching etags in local storage function
     this.$get = function(){
-      return function(_http, _window, _q){
-        var STORAGE_PREFIX = 'eTagKey-';
-
-        // wrapper for localStorage
-        var storage = {};
-        storage.save = function(key, obj){
-          var val = JSON.stringify(obj);
-          _window.localStorage.setItem(key, val);
-        };
-        storage.get = function(key){
-          var val = _window.localStorage.getItem(key);
-          return JSON.parse(val);
-        };
-
-        //generates key to use for localStorage
-        function eTagKey(url){
-          if(!url){ return null; }
-          return STORAGE_PREFIX+url;
-        }
-
-        //creates the object to be cached in localStorage
-        function makeCacheObj(etag, opts, response){
-          return {
-            etag: etag,
-            opts: opts,
-            response: response
-          };
-        }
-
-        //caches the etag to local storage. Won't cache unless url exists in opts
-        function cacheEtag(etag, opts, resp){
-          if(opts && opts.url){
-            if(resp){
-              var cacheResponse = {};
-              angular.extend(cacheResponse, resp);
-              cacheResponse.status = 203;
-
-              storage.save(eTagKey(opts.url), makeCacheObj(etag, opts, cacheResponse) );
-            }
-          }
-        }
-
-        //Wrapper for _http get
-        return function ehttpGet(urlOpts){
-
-          // Handlers for the server response
-          var respFn = {}
-
-          // cache the Etag prior to returning the reponse
-          respFn.cacheEtag = function(resp){
-            var etag = resp.headers().etag;
-            if(etag){ cacheEtag(etag, urlOpts, resp); }
-            return resp;
-          };
-
-          //304's are treated as exceptions in angular, so
-          //catch it, reject to bubble up the error if not 304
-          respFn.catchUnmodified =  function(resp){
-            if(resp.status === 304){
-              cacheObj.response.headers = function(){
-                return {"X-Local-Cache": "Nothing sent to server"};
-              };
-              return cacheObj.response;
-
-            } else {
-              return _q.reject(resp);
-            }
-          };
-
-          var url = urlOpts.url;
-
-          var cacheObj = storage.get(eTagKey(url));
-
-          if(cacheObj && cacheObj.etag){
-            urlOpts.headers = urlOpts.headers || {};
-            angular.extend( urlOpts.headers, {'If-None-Match': cacheObj.etag} );
-          }
-
-          angular.extend(urlOpts, {method: 'GET'});
-
-          //_http decorated with etag
-          //_http.etagGet(urlOpts);
-          return _http(urlOpts)
-            .then(respFn.cacheEtag)
-            .catch(respFn.catchUnmodified);
-        }
-      }
+      return injector.get('etagLocalStorageCacheFn');
     };
   })
 
+// Now we're going to create our $http drop in replacement
 .config(function($provide, etagLocalStorageCacheProvider){
 
+    //Using the decorator we add an initialization method
+    //to $http (the $delegate)
     $provide.decorator('$http', function($delegate){
       $delegate.etagGetInit = etagLocalStorageCacheProvider.$get();
       return $delegate;
@@ -209,7 +133,7 @@ angular.module('AngularEtag', [])
 
   //I don't like this but I'm currently stumped on how to do it better
   // specifically I don't like directly changing $http here
-  // maybe create a function in config/provider that adds the method?
+  // however, $window and $q are defined here.
   $http.etagGet = $http.etagGetInit($http, $window, $q);
 
   return $http;
